a35392f5637f13bf07bbf1baea0fd8f5c8f3a58a
Amora77 <amradelali96@gmail.com>
2023-06-20 03:46:09 -0700

    s

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print numbers
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit <= 10 ; digit++)
+	{
+		printf("%d" , digit)
+	}
+
+	putchar('\n');
+	return (0);
+}
