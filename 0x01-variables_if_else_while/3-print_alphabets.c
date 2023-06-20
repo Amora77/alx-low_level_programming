b1d02224f0804ff7df61e9aef949ef3c9c9dc8a4
Amora77 <amradelali96@gmail.com>
2023-06-20 00:32:42 -0700

    s

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print Alphapit in lowercase
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		putchar(letter);
+	for (letter = 'A'; letter <= 'Z'; letter++)
+		putchar(letter);
+	putchar('\n');
+
+	return (0);
+}
