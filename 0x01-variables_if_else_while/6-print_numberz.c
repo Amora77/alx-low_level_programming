ec60cc7daaf5196511732b7ec7a3405e932191d6
Amora77 <amradelali96@gmail.com>
2023-06-20 03:52:54 -0700

    s

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Print numbers
+ *
+ * Return: 0 (success)
+ */
+
+int main(void){
+
+	int digit;
+
+	for (digit = 0; digit <= 9 ; digit++)
+	{
+		putchar((digit % 10) + '0');
+	}
+
+	putchar('\n');
+	return (0);
+}
