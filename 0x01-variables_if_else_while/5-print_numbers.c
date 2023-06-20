2800f447cd11b4724873b03e9621fa7b94d54d26
Amora77 <amradelali96@gmail.com>
2023-06-20 03:48:15 -0700

    s

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -14,9 +14,9 @@ int main(void)
 {
 	int digit;
 
-	for (digit = 0; digit <= 10 ; digit++)
+	for (digit = 0; digit <= 9 ; digit++)
 	{
-		printf("%d" , digit);
+		printf("%d", digit);
 	}
 
 	putchar('\n');
