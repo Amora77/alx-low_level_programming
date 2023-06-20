2329fdeaf09724a2cbfcf2acaf79072346d43bf8
Amora77 <amradelali96@gmail.com>
2023-06-19 23:29:28 -0700

    s

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,26 +2,20 @@
 #include <time.h>
 #include <stdio.h>
 
-/**
- * main - entry point
- *
- * Description: Print last digit of number
- *
- * Return: 0 (success)
- */
- 
+/*
+* main - entry point
+*
+* Description: Print Alphapit in lowercase
+*
+* Return: 0 (success)
+*/
+
 int main(void)
 {
-	int n, digit;
-        
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	digit = (n%10)
-	if(digit > 5)
-		printf("Last digit of %i is %i and is greater than 5\n", n, digit)	
-	else if(digit == 0)
-	        printf("Last digit of %i is %i and is 0 ", n, digit)
-	else if (digit < 6 && digit != 0)
-                printf("Last digit of %i is %i and is less than 6 and not 0\n", n, digit)		
-	return (0);
+    char letter;
+    for (letter = 'a'; letter <= 'z'; letter++)
+       putchar(letter);
+       putchar('\n');
+    return (0);
 }
+
