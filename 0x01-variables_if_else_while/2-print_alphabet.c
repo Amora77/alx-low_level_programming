002c779611540c4d5c3d56efd6b9f3c2593ff002
Amora77 <amradelali96@gmail.com>
2023-06-20 00:26:02 -0700

    s

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,21 +8,14 @@
  * Description: Print Alphapit in lowercase
  *
  * Return: 0 (success)
- */ 
-  
-int
-main (void) 
-{
-  
-char letter;
-  
- 
-for (letter = 'a'; letter <= 'z'; letter++)
-    
-putchar (letter);
-  
-putchar ('\n');
-  
-return (0);
+ */
 
+int main(void)
+{
+        char letter;
+
+        for (letter = 'a'; letter <= 'z'; letter++)
+            putchar(letter);
+        putchar('\n');
+        return (0);
 }
