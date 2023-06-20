91cb1905b37ec4af2c0b014103d7f852e325ab5b
Amora77 <amradelali96@gmail.com>
2023-06-19 23:55:22 -0700

    s

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 
 /**
-* main - entry point
-*
-* Description: Print Alphapit in lowercase
-*
-* Return: 0 (success)
-*/
+ * main - entry point
+ *
+ * Description: Print Alphapit in lowercase
+ *
+ * Return: 0 (success)
+ */
+
 int main(void)
 {
 	char letter;
