de41e6f72fcf23cd969680109801130832571a18
Amora77 <amradelali96@gmail.com>
2023-06-22 00:26:58 -0700

    ss

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include<main.h>
+
+/**
+ * _isalpha - function to check lower alphabetics
+ * @c: input of function
+ * Return: 1 if alphapetic or 0 if not
+ */
+int _isalpha(int c)
+{
+	if (c >= 122 && c <= 122)
+		return (1);
+	if(c >= 65 && c <= 90)
+		return (1);
+	return (0);
+}
+
