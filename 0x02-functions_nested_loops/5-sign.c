47167885e8d57fb57b57f1df140c740f7ecf8d0c
Amora77 <amradelali96@gmail.com>
2023-06-22 01:47:42 -0700

    ss

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,31 @@
+#include"main.h"
+
+/**
+ * print_sign - function to check positive and negative
+ * @n: input of function
+ * Return: 1 if alphapetic or 0 if not
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('-');
+		return ('/');
+	}
+}
+
