ea99c228b94184a1f14e1fd0f6d409b760fde85e
Amora77 <amradelali96@gmail.com>
2023-06-22 01:50:40 -0700

    ss

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * _abs - function to get absolute value
+ * @n: input of function
+ * Return: 1 if alphapetic or 0 if not
+ */
+int _abs(int i)
+{
+	if (i < 0)
+		i = i * -1;
+	return (i);
+}
