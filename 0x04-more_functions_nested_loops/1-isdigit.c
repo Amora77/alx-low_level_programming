02cc3d28a9861abd173411eaef68b8f9e6567e0d
Amora77 <amradelali96@gmail.com>
2023-06-22 23:56:13 -0700

    ss

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include"main.h"
+
+/**
+ * _isdigit - check for uppercase
+ * Description: print string by putchar
+ * @c: input char
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+        if (c >= 48 && c <= 57)
+                return (1);
+        else
+                return (0);
+}
