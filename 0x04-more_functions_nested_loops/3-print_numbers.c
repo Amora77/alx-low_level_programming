4179d9b80e288b22db5ed8f003025b3e3b42dd0c
Amora77 <amradelali96@gmail.com>
2023-06-23 00:09:19 -0700

    ss

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * print_numbers - check for uppercase
+ * Description: print 1 to 9
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	for(int i = 0 ; i< 10 ; i++)
+ 		_putchar(48 + i)
+
+	_putchar('\n')
+}
