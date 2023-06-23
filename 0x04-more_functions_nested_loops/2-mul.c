f37d33e155ab6f00d958909dc9f85763169a5582
Amora77 <amradelali96@gmail.com>
2023-06-23 00:03:06 -0700

    ss

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * mul - multibly two integers
+ * Description: print string by putchar
+ * @a: input int
+ * @b: input int
+ * Return: 0 or 1
+ */
+int mul(int a, int b)
+{
+	return (a * b)
+}
