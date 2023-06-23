41c4acec3443dc9c58932c50f2ec2a104defbca6
Amora77 <amradelali96@gmail.com>
2023-06-23 01:54:16 -0700

    ss

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print prime number
+ * Return: 0 ore one 
+ */
+int main(void)
+{
+	long i;
+	long num = 612852475143;
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			num /= i;
+	}
+	printf("%li\n", num);
+	return (0);
+}
