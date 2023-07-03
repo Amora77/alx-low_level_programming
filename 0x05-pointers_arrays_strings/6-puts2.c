fec9f8c9da46920e62a01ad02004044b70d87bf9
Amora77 <amradelali96@gmail.com>
2023-07-03 01:04:43 -0700

    s

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -7,14 +7,16 @@
  */
 void puts2(char *str)
 {
-	int len = 0;
+	int len, i;
+
+	len = 0;
 
 	while (str[len] != '\0')
 	{
 		len++;
 	}
 
-	for (int i = 0; i < len; i += 2)
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
 	}
