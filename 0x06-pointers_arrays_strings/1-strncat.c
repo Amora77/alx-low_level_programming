497a53b8666e143c7acd8a62ce7d7ee35247f084
Amora77 <amradelali96@gmail.com>
2023-07-04 01:34:11 -0700

    ss

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *_strcat - concatination function.
+ *
+ *@dest:  dest
+ *@src: source
+ *
+ * Return: This return to dest.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		;
+	for (j = 0; j <= n && src[j] != '\0'; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	if (j < n)
+	{
+	dest[i] = '\0';
+	}
+	return (dest);
+}
